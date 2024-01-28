1d64ea786a48e083751a6307dd83ebe10ff8ce00
teliao <xxoooxx345678@gmail.com>
2024-01-29 00:24:08 +0800

    fix rng

--- rng.h
+++ rng.h
@@ -11,7 +11,7 @@ int16_t norm_rand(int16_t mean, int16_t std, int16_t min, int16_t max)
     double u = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
     double v = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
 
-    double tmp = sqrt(-2.0 * log(fabs(u))) * sin(2.0 * PI * v);
+    double tmp = sqrt(-2.0 * log(fabs(u))) * cos(2.0 * PI * v);
     int16_t ret = mean + (int16_t)(tmp * std);
 
     return (ret < min ? min : (ret > max ? max : ret));
